9270060b6a18c67584be2e443a30813e108ff9a3
lyy76 <108745731+lyy76@users.noreply.github.com>
2023-12-03 22:43:54 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,63 @@
+const hamburger = document.querySelector('.hamburger'),
+    menu = document.querySelector('.menu'),
+    closeElem = document.querySelector('.menu__close'),
+    list = document.querySelector('.menu__list');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+list.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+const counters = document.querySelectorAll('.skills__ratings-counter'),
+    lines = document.querySelectorAll('.skills__ratings-line span');
+
+counters.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
+
+$(document).ready(function () {
+    $('form').submit(function (e) {
+        e.preventDefault();
+
+        /*  if (!$(this).valid()) {
+            return;
+         } */
+
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function () {
+            $(this).find("input").val("");
+            /* $('#contacts, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow'); */
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1400) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href=#up]").click(function () {
+        const _href = $(this).attr("href");
+        $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+        return false;
+    });
+
+    new WOW().init();
+
+}); 
