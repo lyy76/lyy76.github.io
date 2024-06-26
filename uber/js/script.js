353d0665226d6d1e2699f50cc3cf8c6b67adb6bc
lyy76 <108745731+lyy76@users.noreply.github.com>
2024-06-26 19:58:03 +0300

    Add files via upload

--- uber/js/script.js
+++ uber/js/script.js
@@ -15,3 +15,87 @@ window.addEventListener('DOMContentLoaded', () => {
         })
     })
 })
+
+//Modal
+
+$('[data-modal=consultation]').on('click', function () {
+    $('.overlay, #consultation').fadeIn('slow');
+});
+
+$('[data-modal=order]').on('click', function () {
+    $('.overlay, #order').fadeIn('slow');
+});
+
+$('.modal_close').on('click', function () {
+    $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+});
+
+
+function validateForms(form) {
+    $(form).validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2
+            },
+            phone: "required",
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            name: {
+                required: "введите свое имя",
+                minlength: jQuery.validator.format("Введите {0} символа!")
+            },
+            phone: "введите свой номер",
+            email: {
+                required: "введите свою почту",
+                email: "email в формате name@domain.com"
+            }
+        }
+    });
+};
+
+validateForms('#consultation form');
+validateForms('#order form');
+
+$('input[name=phone]').mask("+38 (999) 999-99-99");
+
+$('form').submit(function (e) {
+    e.preventDefault();
+
+    /*  if (!$(this).valid()) {
+        return;
+     } */
+
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function () {
+        $(this).find("input").val("");
+        $('#consultation, #order').fadeOut();
+        $('.overlay, #thanks').fadeIn('slow');
+
+        $('form').trigger('reset');
+    });
+    return false;
+});
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 1000) {
+        $('.pageup').fadeIn();
+    } else {
+        $('.pageup').fadeOut();
+    }
+});
+
+$("a[href=#up]").click(function () {
+    const _href = $(this).attr("href");
+    $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+    return false;
+});
+
+new WOW().init();
